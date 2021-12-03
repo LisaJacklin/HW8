284be1736ae9f20809401f41b836394da4a0d8a4
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-12-02 19:34:59 -0900

    ceasarimprove.cpp original encryption code

--- ceasarimprove/ceasarimprove.cpp
+++ ceasarimprove/ceasarimprove.cpp
@@ -1,10 +1,40 @@
 // ceasarimprove.cpp 
-//
+//Lisa Jacklin
+//CS 201
+//12/1/2021
+
+//improved version of the ceasar-shift.cpp from HW7
+
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    char message[100], ch;
+    int i, key;
 
+
+    std::cout << "Enter a message to encrypt: ";
+    std::cin.getline(message, 100);
+    std::cout << "Enter key: ";
+    std::cin >> key;
+    for (i = 0; message[i] != '\0'; ++i) {
+        ch = message[i];
+        if (ch >= 'a' && ch <= 'z') {
+            ch = ch + key;
+            if (ch > 'z') {
+                ch = ch - 'z' + 'a' - 1;
+            }
+            message[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + key;
+            if (ch > 'Z') {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            message[i] = ch;
+        }
+    }
+    std::cout << "Encrypted message: " << message;
+    return 0;
+}
