627bb6d4bfaa5a1773f7ecdf15c6ec626a940856
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-12-02 21:42:56 -0900

    addprog2.cpp vector max and min functions

--- addprog2/addprog2.cpp
+++ addprog2/addprog2.cpp
@@ -1,20 +1,57 @@
-// addprog2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// addprog2.cpp 
+// Lisa Jacklin
+// CS 201
+// 12/1/2021
 //
 
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct vectorStats {
+    double min = 0.0;
+    double max = 0.0;
+    double mean = 0.0;
+    double median = 0.0;
+};
+
+void maxVal(std::vector <int>& num);
+void minVal(std::vector <int>& num);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector <int> s1{ 1, 26,7, 14,5,2 };
+    std::vector <int> s2{ 5,10,15,20, 25,30 };
+    std::vector <int> s3{ 16, 0, 14, 43, 78, 19 };
+    std::vector <int> s4{ 2, 10, 14, 17, 0, 22 };
+    std::vector <int> s5{ 0,2,17,0,15, 29 };
+
+    maxVal(s1);
+    minVal(s1);
+    maxVal(s2);
+    minVal(s2);
+    maxVal(s3);
+    minVal(s3);
+    maxVal(s4);
+    minVal(s4);
+    maxVal(s5);
+    minVal(s5);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//return all min, max, mean and meadian values using a struct.
+
+void maxVal (std::vector <int> & num) {
+//finding the maximum element
+    int max = *max_element(num.begin(), num.end());
+    std::cout << "max: " << max << "\n";
+}
+
+void minVal (std::vector <int> & num) {
+
+    int min = *min_element(num.begin(), num.end());
+    std::cout << "min: " << min << "\n";
+}
+
+
